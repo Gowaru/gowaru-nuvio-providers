5bcb0f22b07577463ef93207dddef9246e2cde8d
Gowaru <amemawuli8@gmail.com>
2026-05-19 22:49:34 +0000

    Refactor code structure for improved readability and maintainability

--- src/utils/resolvers.js
+++ src/utils/resolvers.js
@@ -5,6 +5,7 @@
 
 const HEADERS = {
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/145.0.0.0 Safari/537.36",
+    "Accept-Encoding": "identity",
 };
 
 const _atob = (str) => {
